ce06257c45b5a143c26341f87fcd6553f451d736
MarcosFernandesF <marcosrff15@gmail.com>
2023-09-25 19:41:15 -0300

    Adicionando lógica de intervalo de bipts

--- proximityAlert/proximityAlert.ino
+++ proximityAlert/proximityAlert.ino
@@ -12,12 +12,14 @@ const int triggerPin = 9;
 const int echoPin = 10;
 const int buzzerPin = 8;
 
+// Define os pinos dos botões que vao incrementar e decrementar a distância.
+
 // Variáveis para armazenar o tempo de ida e volta do ultrassom
 long duration;
 int distance;
 
 // Distância limite para acionar o buzzer.
-int max_distance = 5;
+int max_distance = 10;
 
 void setup() {
 
@@ -27,6 +29,7 @@ void setup() {
   pinMode(triggerPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(buzzerPin, OUTPUT);
+
 
   // Desliga o buzzer no início
   digitalWrite(buzzerPin, LOW);
@@ -44,30 +47,57 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(triggerPin, LOW);
 
-  // Mede a duração do pulso Echo
+  // Mede a duração do pulso.
   duration = pulseIn(echoPin, HIGH);
 
   // Calcula a distância em centímetros
   distance = duration * 0.034 / 2;
 
+  // Display LCD.
+  // Dist: xxcm
+  // Limite: xxcm
   lcd.setBacklight(HIGH);
   lcd.setCursor(0, 0);
-  lcd.print("Distancia:");
-  lcd.setCursor(6, 1);
+  lcd.print("Distance: ");
   lcd.print(distance);
-  lcd.setCursor(12, 1);
+  lcd.print("cm");
+  lcd.setCursor(0, 1);
+  lcd.print("Limit: ");
+  lcd.print(max_distance);
   lcd.print("cm");
 
-  // Verifica se a distância é menor que um valor de limite (por exemplo, 20 cm)
+  // Lê o valor do potenciômetro e o usa como o novo valor de max_distance
+  int potValue = analogRead(A5);
+  max_distance = map(potValue, 0, 1023, 0, 100);
+
+  // De acordo com a distância muda a frequência dos bips.
+  int num_bips = 0;
   if (distance < max_distance) {
-    // Se a distância for menor que 20 cm, ativa o buzzer
-    digitalWrite(buzzerPin, HIGH);
-  } else {
-    // Caso contrário, desliga o buzzer
-    digitalWrite(buzzerPin, LOW);
+    int frequency = 528;
+    if (distance < max_distance / 4) {
+      num_bips = 1;
+    } 
+    else if (distance < max_distance / 2) {
+      num_bips = 3;
+      frequency = 392;
+    } 
+    else {
+      num_bips = 5;
+      frequency = 100;
+    }
+      tone(buzzerPin, frequency);
+  } 
+  else {
+    noTone(buzzerPin);
   }
 
-  // Aguarda um curto período de tempo antes de fazer a próxima leitura
-  delay(1000);
+  // Emitir o som com base no número de bips
+  for (int i = 0; i < num_bips; i++) {
+    delay(25);
+    noTone(buzzerPin);
+    delay(25);
+  }
+
+  delay(200);
   lcd.clear();
 }
